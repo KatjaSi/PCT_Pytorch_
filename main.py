592857b9fab07889ee1065403b03ed90f2546d7a
KatjaSi <katja.sivertsen@gmail.com>
2023-09-16 10:18:41 +0200

    trained one more model

--- main.py
+++ main.py
@@ -127,7 +127,7 @@ def train(args, io):
         io.cprint(outstr)
         if test_acc >= best_test_acc:
             best_test_acc = test_acc
-            torch.save(model.state_dict(), 'checkpoints/%s/models/model.t7' % args.exp_name)
+            torch.save(model.state_dict(), 'checkpoints/%s/models/model_pct.t7' % args.exp_name)
 
 
 def test(args, io):
